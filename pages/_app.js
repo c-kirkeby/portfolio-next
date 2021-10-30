6c8e91701266c2a793c2077d5b8d9b8351bbe936
ckirkeby <christian@christiankirkeby.com>
2021-10-30 17:03:21 +1000

    [WIP] Add TinaCMS

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,32 @@
-import App from 'next/app'
+import dynamic from 'next/dynamic'
 import withDarkMode, { MODE } from 'next-dark-mode'
-
+import { TinaEditProvider } from 'tinacms/dist/edit-state'
 import '../styles/global.css'
+const TinaCMS = dynamic(() => import('tinacms'), { ssr: false })
 
-export default withDarkMode(App, { defaultMode: MODE.DARK })
+
+const App = ({ Component, pageProps }) => {
+  return (
+    <>
+      <TinaEditProvider
+        editMode={
+          <TinaCMS
+            clientId={process.env.NEXT_PUBLIC_TINA_CLIENT_ID}
+            branch={process.env.NEXT_PUBLIC_EDIT_BRANCH}
+            organization={process.env.NEXT_PUBLIC_ORGANIZATION_NAME}
+            isLocalClient={Boolean(
+              Number(process.env.NEXT_PUBLIC_USE_LOCAL_CLIENT ?? true)
+            )}
+            {...pageProps}
+          >
+            {(livePageProps) => <Component {...livePageProps} />}
+          </TinaCMS>
+        }
+      >
+        <Component {...pageProps} />
+      </TinaEditProvider>
+    </>
+  )
+}
+
+export default App
