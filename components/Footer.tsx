77e0b5cd6c0b79a8c4f97a82f2a68c3c72476410
c-kirkeby <christian@christiankirkeby.com>
2021-11-20 15:36:10 +1000

    Change footer to reflect the new tech choice

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ import styles from './Footer.module.css'
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <p>Made using <a href="https://reactjs.org/">React</a> and <a href="https://www.gatsbyjs.org/">Gatsby</a>.</p>
+      <p>Made using <a href="https://reactjs.org/">React</a> and <a href="https://www.nextjs.org/">Next.js</a>.</p>
     </footer>
   )
 }
